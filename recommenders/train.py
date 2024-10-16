e336e9b9a3c90e3fc2df7fa33f1efb3322e7c965
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2024-10-16 17:37:55 +0200

    Changed Bert4Rec training parameters

--- recommenders/train.py
+++ recommenders/train.py
@@ -2,10 +2,16 @@ from recbole.config import Config
 from recbole.data import create_dataset, data_preparation
 from recbole.trainer import Trainer
 from recbole.utils import get_model
+from recbole.utils import init_seed
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
+#Bert4Rec hyperparameters taken from https://github.com/asash/bert4rec_repro
 parameter_dict = {
         'load_col': {"inter": ['user_id', 'item_id', 'rating', 'timestamp']},
-        'train_neg_sample_args': None
+        "loss_type": "BPR",
+        "train_neg_sample_args":{'distribution': 'uniform', 'sample_num': 100},
+        "train_batch_size": 12288,
         }
 
 config = Config(model='BERT4Rec', dataset='ml-1m', config_dict=parameter_dict)
@@ -23,8 +29,8 @@ model = get_model(config['model'])(config, train_data.dataset).to(config['device
 
 # Perform inference
 trainer = Trainer(config, model)
-latest_checkpoint = "saved/Bert4Rec_ml1m.pth"
-trainer.resume_checkpoint(latest_checkpoint)
-# results = trainer.fit(train_data, show_progress=True)
-results = trainer.evaluate(test_data, show_progress=True, model_file=latest_checkpoint)
+# latest_checkpoint = "saved/Bert4Rec_ml1m.pth"
+# trainer.resume_checkpoint(latest_checkpoint)
+results = trainer.fit(train_data, show_progress=True)
+# results = trainer.evaluate(test_data, show_progress=True, model_file=latest_checkpoint)
 print(results)
