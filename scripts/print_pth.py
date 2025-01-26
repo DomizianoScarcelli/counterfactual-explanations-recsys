1d8360ed2bc61290c681ba8bf44fb57b567b08c5
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2025-01-26 14:29:19 +0100

    Added scripts for genetic hyperopt with split

--- scripts/print_pth.py
+++ scripts/print_pth.py
@@ -1,8 +1,9 @@
 import torch
 import fire
+from config import ConfigParams
 
 def print_pth(pth_path:str):
-    pth_file = torch.load(pth_path)
+    pth_file = torch.load(pth_path, map_location=ConfigParams.DEVICE)
     print(pth_file)
 
 if __name__ == "__main__":
