b9bf68a192d3adec7d23b35b9ebcee2663452353
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2025-03-22 15:39:03 +0100

    minor bug fix in dataset indices

--- utils/generators.py
+++ utils/generators.py
@@ -451,9 +451,6 @@ class TimedGenerator:
             item = next(self.generator)
         except StopIteration:
             raise
-        except KeyError:
-            self.times.append(0)
-            raise
         elapsed_time = time.time() - start_time
         self.times.append(elapsed_time)
         return item
