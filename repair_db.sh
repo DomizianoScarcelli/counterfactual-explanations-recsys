033617d714a32575a319c7f6e1649643d5aab100
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2025-03-27 19:30:37 +0100

    Minor bug fix

--- repair_db.sh
+++
@@ -1,7 +0,0 @@
-DATABASE_LOCATION="results/evaluate"
-DATABASE_NAME="alignment.db"
-
-cd $DATABASE_LOCATION
-echo '.dump'|sqlite3 $DB_NAME|sqlite3 repaired_$DB_NAME
-mv $DB_NAME corrupt_$DB_NAME
-mv repaired_$DB_NAME $DB_NAME
